993b9f36722d1fff2e8af7ce306f28130f856f0d
hgf748h4g78fh48fg4478hfg487h874gf841h4ghfh487gf <45438302+458hgf456hggh45fh56f4hgf546hgfhf@users.noreply.github.com>
2019-06-03 22:29:31 +0200

    Update BotMain.js

--- .gitignore/BotMain.js
+++ .gitignore/BotMain.js
@@ -992,13 +992,24 @@ switch (args[0].toLowerCase()) {
 		}
   }
 	
-if(message.content.startsWith(prefix + "an"){
+  if(message.content.startsWith(prefix + "an")){
 
-  if(message.author.id === "253911060954742784"){
-	message.channels.get("518815172890984456").send("TEST")
+  if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply("Vous n'avez pas la permission `Administrateur`");
 
-}
- 
+  var args = message.content.split(' ').join(' ').slice(5);
+
+  if(!args) return message.reply("Veuillez marquer une phrase ou un mot !")
+
+  var say_embed = new Discord.RichEmbed()
+
+  .setDescription(`${args}`)
+
+  .setFooter("Envoyé par " + message.author.username)
+
+  message.delete()
+
+  message.channels.get("518815172890984456 ").send(say_embed);
+
 }
 
 });
