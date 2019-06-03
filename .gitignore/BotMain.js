bd0b28ce36d53946d91376b7727beee260409e4c
hgf748h4g78fh48fg4478hfg487h874gf841h4ghfh487gf <45438302+458hgf456hggh45fh56f4hgf546hgfhf@users.noreply.github.com>
2019-06-03 22:14:59 +0200

    Update BotMain.js

--- .gitignore/BotMain.js
+++ .gitignore/BotMain.js
@@ -100,7 +100,7 @@ client.on('message', async message => {
       .setFooter("By CuBz#7372")
 
       message.channel.send(aide_embed);
-	    client.channels.get("518815172890984456").send("OversightBOT en ligne !")
+	    message.channels.get("518815172890984456").send("OversightBOT en ligne !")
 
     }
 
